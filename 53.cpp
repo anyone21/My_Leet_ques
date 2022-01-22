70e8acf937539cf739d11175c6f93b2dbb857ac1
annoying_code <32765126+anyone21@users.noreply.github.com>
2022-01-22 13:49:53 +0530

    Update 53.cpp

--- 53.cpp
+++ 53.cpp
@@ -2,15 +2,15 @@
 // Kadane's Algorithm is an O ( n ) {\displaystyle O(n)} {\displaystyle O(n)} algorithm for finding the maximum contiguous subsequence in a one-dimensional sequence. 
 
 int maxSubArray(vector<int>& nums) {
+        
         int currentMaxSum = nums[0];
         int maxSum = currentMaxSum;
         int maxStartIndex = 0, maxEndIndex = 0;
-        // int  = 0;
         int currentStartIndex = 0;
-        
+
         for(int currentEndIndex = 1; currentEndIndex<nums.size();currentEndIndex++){
             currentMaxSum = max(nums[currentEndIndex],currentMaxSum+nums[currentEndIndex]);
-            
+
             if(currentMaxSum>maxSum){
                 maxSum = currentMaxSum;
                 maxStartIndex = currentStartIndex;
@@ -21,8 +21,26 @@ int maxSubArray(vector<int>& nums) {
                 currentStartIndex = currentEndIndex+1;
             }
         }
-        
-        // cout<<maxStartIndex<<", "<<maxEndIndex<<endl;
         return maxSum;
+}
+
+
+// Without starting and ending index of that subarray
+
+int maxSubArray(vector<int>& nums) {
         
-    }
+        int currentMaxSum = nums[0];
+        int maxSum = currentMaxSum;
+
+        for(int currentEndIndex = 1; currentEndIndex<nums.size();currentEndIndex++){
+            currentMaxSum = max(nums[currentEndIndex],currentMaxSum+nums[currentEndIndex]);
+
+            if(currentMaxSum>maxSum){
+                maxSum = currentMaxSum;
+            }
+            if(currentMaxSum <0){
+                currentMaxSum = 0;
+            }
+        }
+        return maxSum;
+}
