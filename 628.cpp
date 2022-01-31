0625ad13c93ff1c611efb42806236930080376d2
annoying_code <32765126+anyone21@users.noreply.github.com>
2022-01-31 18:18:54 +0530

    Update 628.cpp

--- 628.cpp
+++ 628.cpp
@@ -33,4 +33,80 @@ public:
 };
 
 // Above approch is not suitable for Maximum products of K numbers
+// It's a DP approch 
+// We are considering K = 3
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int k = 3;
+        vector<vector<int>> dpProd;
+        
+        dpProd[0][0] = 1;
+        dpProd[1][0] = 1;
+        
+        for(int i =1;i<=k;i++){
+            dpProd[0][i] = INT_MIN;
+            dpProd[1][i] = INT_MAX;
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            // pending
+        }
+    }
+};
 
+
+/*
+ 0   ......   k+1
+ ----------------
+|     |     |    |  --> MaxProduct
+ ----------------
+|     |     |    |  --> MinProduct
+ ----------------
+ 
+*/
+
+
+// Using maxHeap and minHeap
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int k = 3;
+        // Here we make a maxHeap and minHeap that store K largest and smallest number
+        priority_queue<int> maxQ(nums.begin(),nums.end());  // max_heap
+        priority_queue<int,vector<int>,greater<int>> minQ(nums.begin(),nums.end());  //min_heap
+        
+        int maxNum = maxQ.top();
+        int count = 0;
+        int minP = 1;
+        int maxP = 1;
+        
+        if(k%2!=0){
+            while(count!=k-1){
+                minP *= minQ.top();
+                minQ.pop();
+                count++;
+            }
+        }else{
+            while(count!=k){
+                minP *= minQ.top();
+                minQ.pop();
+                count++;
+            }
+        }
+
+        
+        if(k%2!=0){
+            minP *= maxNum;
+        }
+        
+        count = 0;
+        while(count!=k){
+            maxP = maxP*maxQ.top();
+            maxQ.pop();
+            count++;
+        }
+
+        return max(maxP,minP);
+    }
+};
